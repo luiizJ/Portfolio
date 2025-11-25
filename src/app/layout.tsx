d5d3cafd712b7752dacc1d8635782ae0c3f4f5d4
luizevrs <luiizevrsj@gmail.com>
2025-11-24 21:47:27 -0300

    chore(seo): update metadata title, description and open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,26 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "",
+  title: "Luizj | Software Developer",
+  description:
+    "Portfólio de Luiz Janampa. Desenvolvedor de Software especializado em Frontend e aplicações web modernas utilizando React, Next.js e ecossistema Node.js.",
+  keywords: [
+    "Frontend",
+    "React",
+    "Next.js",
+    "Developer",
+    "Software Engineer",
+    "Portfólio",
+    "Luiz Janampa",
+  ],
+  authors: [{ name: "Luiz Janampa" }],
+  openGraph: {
+    title: "Luiz | Software Developer",
+    description: "Confira meu portfólio e projetos de alta performance.",
+    type: "website",
+    locale: "pt_BR",
+    url: "https://portfolio-two-murex-39.vercel.app/",
+  },
 };
 
 export default function RootLayout({
@@ -13,9 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
