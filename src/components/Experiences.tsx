71cbc0c3824aa2d17cb2d85df1260266d8089e1a
luizevrs <luiizevrsj@gmail.com>
2025-11-24 21:38:09 -0300

    fix(core): correct logic error in Experiences / Main / Projects

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -1,40 +1,63 @@
-"use client"
-import { containetExperienceAni, headerExperienceAni } from "@/animation/Motion";
+"use client";
+import {
+  containetExperienceAni,
+  headerExperienceAni,
+} from "@/animation/Motion";
 import { EXPERIENCES } from "@/constants";
 import { motion } from "framer-motion";
 
-export const Experiences = () =>{
-  return ( 
+const Experiences = () => {
+  return (
     <div className="border-b border-neutral-900 pb-4">
-      <motion.h2 {...headerExperienceAni} className="my-20 text-center text-4xl">Experiencias</motion.h2>
+      <motion.h2
+        {...headerExperienceAni}
+        className="my-20 text-center text-4xl"
+      >
+        Experiencias
+      </motion.h2>
+
       <div>
-        {EXPERIENCES.map((exp) =>(
-          <motion.div 
-          {...containetExperienceAni}
-          key={exp.id} 
-          className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
-            <p className="mb-2 text-sm text-neutral-400">{exp.year}</p>
-            </div>
-            <div className="w-full max-w-2xl lg:w-3/4">
-            <h6 className=" mb-2 font-semibold"> 
-              {exp.role} - <span className="text-sm text-purple-200">{exp.company}</span>
-            </h6>
-            <p className="mb-4 text-neutral-400">{exp.description}</p>
-            <div className="flex flex-wrap">
-            {exp.technologies.map((techno,index)=>(
-              <span key={index} className="mr-2 mt-4 rounded bg-purple-700 px-2
-                py-1 text-sm font-medium text-neutral-900">
-                   {techno}
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <motion.div
+              {...containetExperienceAni}
+              className="flex flex-wrap w-full lg:justify-center" // Repassamos classes de layout interno se necessário
+            >
+              <div className="w-full lg:w-1/4">
+                <p className="mb-2 text-sm text-neutral-400">
+                  {experience.year}
+                </p>
+              </div>
+
+              <div className="w-full max-w-2xl lg:w-3/4">
+                <h6 className="mb-2 font-semibold">
+                  {experience.role} -{" "}
+                  <span className="text-sm text-purple-200">
+                    {experience.company}
+                  </span>
+                </h6>
+
+                <p className="mb-4 text-neutral-400">
+                  {experience.description}
+                </p>
+
+                <div className="flex flex-wrap">
+                  {experience.technologies.map((tech, techIndex) => (
+                    <span
+                      key={techIndex}
+                      className="mr-2 mt-4 rounded bg-purple-700 px-2 py-1 text-sm font-medium text-neutral-900"
+                    >
+                      {tech}
                     </span>
-            ))}
-            </div>
-            </div>
-          </motion.div>
+                  ))}
+                </div>
+              </div>
+            </motion.div>
+          </div>
         ))}
       </div>
     </div>
-  )
+  );
 };
 
 export default Experiences;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { aboutAni, headerExperienceAni } from "@/animation/Motion";
-import { PROJECTS } from "@/constants"
+import { PROJECTS } from "@/constants";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,43 +8,56 @@ import Link from "next/link";
 export const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-10">
-      <motion.h2 {...headerExperienceAni} className="my-20 text-center text-4xl">Projetos</motion.h2>
+      <motion.h2
+        {...headerExperienceAni}
+        className="my-20 text-center text-4xl"
+      >
+        Projetos
+      </motion.h2>
+
       <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-        {PROJECTS.map((proj) => (
-          <motion.div 
-          {...aboutAni}
-            key={proj.id} 
-            className="flex flex-col items-center rounded-lg border border-neutral-800 p-4">
-           <Link href={proj.link}>
-           <Image
-              src={proj.image}
-              width={350}
-              height={450}
-              alt={proj.title}
-              className="mb-4 rounded"
-            />
-           </Link>
-            <h6 className="mb-2 text-lg font-semibold text-white">
-              {proj.title}
-            </h6>
-            <p className="mb-4 text-sm text-neutral-400 text-center">
-              {proj.description}
-            </p>
-            <div className="flex flex-wrap justify-center gap-2">
-              {proj.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="rounded bg-purple-700 px-2 py-1 text-sm font-medium text-neutral-900"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div>
-          </motion.div>
+        {PROJECTS.map((proj, index) => (
+          <div key={index} className="h-full">
+            <motion.div
+              {...aboutAni}
+              className="flex flex-col h-full items-center rounded-lg border border-neutral-800 p-4"
+            >
+              <Link href={proj.link} target="_blank" rel="noopener noreferrer">
+                {" "}
+                <Image
+                  src={proj.image}
+                  width={350}
+                  height={450}
+                  alt={proj.title}
+                  className="mb-4 rounded"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Link>
+
+              <h6 className="mb-2 text-lg font-semibold text-white">
+                {proj.title}
+              </h6>
+
+              <p className="mb-4 text-sm text-neutral-400 text-center flex-grow">
+                {proj.description}
+              </p>
+
+              <div className="flex flex-wrap justify-center gap-2 mt-auto">
+                {proj.technologies.map((tech, techIndex) => (
+                  <span
+                    key={techIndex}
+                    className="rounded bg-purple-700 px-2 py-1 text-sm font-medium text-neutral-900"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </motion.div>
+          </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
